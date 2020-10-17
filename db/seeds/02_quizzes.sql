64f1f218849a91e7674d2f102beec9ee58598185
Will <wdportman@gmail.com>
2020-10-17 19:12:34 +0000

    Add seed data for possible_answers table and change double-quotes to single-quotes for 02, 03, and 04 seeds files

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,5 +1,5 @@
 INSERT INTO quizzes (creator_id, title, description, created_at, is_public)
 VALUES
-(7, "Country Capitals Quiz", "Can you select the correct capital for a given country?", '2020-10-01 00:00:00', true),
-(35, "Harry Potter Trivia", "For all of the Potterheads out there", '2020-10-10 12:00:00', false),
-(38, "US Presidents", "How well do you know US presidents? Test your skills with this quiz.", '2020-10-15 10:30:30', true);
+(7, 'Country Capitals Quiz', 'How well do you know country capitals?', '2020-10-01 00:00:00', true),
+(35, 'Harry Potter Trivia', 'For all of the Potterheads out there', '2020-10-10 12:00:00', false),
+(38, 'US Presidents', 'Test your presidential knowledge with this quiz.', '2020-10-15 10:30:30', true);

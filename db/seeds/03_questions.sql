cdaaca031cde99d5ddd2c5a1a9590e5c38dc651c
Will <wdportman@gmail.com>
2020-10-17 18:44:06 +0000

    Add seed data for quizzes and questions tables

---
+++ db/seeds/03_questions.sql
@@ -0,0 +1,11 @@
+INSERT INTO questions (quiz_id, question_num, text)
+VALUES
+(1, 1, "What is the capital of Mexico?"),
+(1, 2, "What is the capital of the United Kingdom?"),
+(1, 3, "What is the capital of Japan?"),
+(2, 1, "Who is the author of Harry Potter?"),
+(2, 2, "Which of the following is a Harry Potter house?"),
+(3, 1, "Who was the heaviest president?"),
+(3, 2, "Who was the only unmarried president?"),
+(3, 3, "Who was the oldest president in office?"),
+(3, 4, "Who was the first president?");

ced5c4d1e718c72c39e767ccdd1fd0657ff56457
Sherwin <sherwin.kwan92@gmail.com>
2020-10-16 23:09:54 -0700

    begin writing queries to create db tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,26 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS quizzes CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS possible_answers CASCADE;
+DROP TABLE IF EXISTS attempts CASCADE;
+DROP TABLE IF EXISTS user_answers CASCADE;
+
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  fname VARCHAR(50),
+  lname VARCHAR(50)
 );
+
+CREATE TABLE quizzes (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP,
+  isPublic BOOLEAN
+);
+
+CREATE TABLE 

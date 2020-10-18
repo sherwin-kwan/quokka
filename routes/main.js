189d7b498cd2fdf6a2861d88d65d363d2bda5e4e
Sherwin <sherwin.kwan92@gmail.com>
2020-10-17 17:02:18 -0700

    quiz routes now work with dummy pages

--- routes/main.js
+++ routes/main.js
@@ -6,17 +6,23 @@
 const express = require('express');
 const router  = express.Router();
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-router.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+const mainRouter = (db) => {
+  // Home page
+  // Warning: avoid creating more routes in this file!
+  // Separate them into separate routes files (see above).
+  router.get("/", (req, res) => {
+    res.render("index.ejs");
+  });
 
-// Catch-all code for non-existent routes. If the route doesn't match anything above, it will send this 404 page:
-router.use((req, res) => {
-  res.status(404).render('error');
-  const templateVars = new Object;
-  templateVars.message = `The page you're looking for could not be found.`;
-  res.render('error.ejs', templateVars);
-});
+  // Catch-all code for non-existent routes. If the route doesn't match anything above, it will send this 404 page:
+  router.use((req, res) => {
+    res.status(404).render('error');
+    const templateVars = new Object;
+    templateVars.message = `The page you're looking for could not be found.`;
+    res.render('error.ejs', templateVars);
+  });
+
+  return router;
+};
+
+module.exports = mainRouter;
--- routes/quiz.js
+++ routes/quiz.js
@@ -7,18 +7,35 @@ const express = require('express');
 const router  = express.Router();
 
 const quizRouter = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+
+  // Load a quiz asynchronously
+  router.get('/:id/load', (req, res) => {
+    const quizId = req.params.id;
+    // db.query(/* INSERT QUERY TO GET QUIZZES */)
+    //   .then(data => {
+    //     const users = data.rows;
+    //     res.json({ users });
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
+  })
+
+  // Display quiz page (page B) - this will instead render a template once that file is done
+  router.get("/:id", (req, res) => {
+    const quizId = req.params.id;
+    res.send(`This is the future home of the quiz page for quiz ${quizId}.
+    <form action='${req.params.id}' method='POST'><button type='submit'>Submit</button></form>`);
   });
+
+  // Submit a quiz
+  router.post('/:id', (req, res) => {
+    const quizId = req.params.id;
+    res.send(`You just successfully submitted quiz ${quizId}`);
+  });
+
   return router;
 };
 

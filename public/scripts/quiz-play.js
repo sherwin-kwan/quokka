d41666d3d48fb508da5d571a4a30f574a29c1e12
Sherwin <sherwin.kwan92@gmail.com>
2020-10-19 11:43:47 -0700

    change button.scss file so we can make elements other than standard HTML buttons look like butons

---
+++ public/scripts/quiz-play.js
@@ -0,0 +1,7 @@
+// All functions should be placed within the $(...) below, this ensures that it only runs when the page has fully loaded
+// Failure to do so may result in asynchronous problems (i.e. the script attempts to create an event listener
+// for a button too early, and since the button hasn't loaded yet, there is an error and when the button finally loads, it's too late to be useful.)
+
+$(() => {
+
+})

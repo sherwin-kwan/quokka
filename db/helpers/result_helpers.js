3a511a5f81f8e08af9dc5a02f0c207d243c24eab
Will <wdportman@gmail.com>
2020-10-19 05:30:43 +0000

    Got basic question display

--- db/helpers/result_helpers.js
+++ db/helpers/result_helpers.js
@@ -3,43 +3,61 @@
 const getAttemptData = function(attemptId, db) {
   const queryString = `
     SELECT
-      quizzes.title,
-      users.fname,
-      users.lname,
-      ROUND((SUM(CASE WHEN possible_answers.is_correct = true THEN 1 ELSE 0 END)/1.00) / (count(user_answers.*)/1.00)*100) as percent_correct
+      quizzes.title AS title,
+      users.fname AS fname,
+      users.lname AS lname,
+      ROUND((SUM(CASE WHEN possible_answers.is_correct = true THEN 1 ELSE 0 END)/1.00) / (count(user_answers.*)/1.00)*100) AS percent_correct,
+      quizzes.id AS quiz_id
     FROM attempts
       JOIN quizzes ON attempts.quiz_id = quizzes.id
       JOIN users ON attempts.user_id = users.id
       JOIN user_answers ON attempts.id = user_answers.attempt_id
       JOIN possible_answers ON user_answers.selected_answer = possible_answers.id
     WHERE attempts.id = $1
-    GROUP BY quizzes.title, users.fname, users.lname, attempts.finished_at
+    GROUP BY quizzes.title, quizzes.id, users.fname, users.lname, attempts.finished_at
   `;
   const queryParams = [attemptId];
   return db.query(queryString, queryParams)
     .then(res => res.rows[0])
-    .catch(err => console.log(err))
+    .catch(err => console.error('query error', err.stack));
 };
 
-const getQuestionData = function(attemptId, db, callback) {
-    const queryString = `
-    SELECT questions.id, question_num, text
-    FROM questions
-      JOIN quizzes ON questions.quiz_id = quizzes.id
-      JOIN attempts on quizzes.id = attempts.quiz_id
-    WHERE attempts.id = $1
-    ORDER BY question_num
-    `;
-    const queryParams = [attemptId];
-    return db.query(queryString, queryParams, (err, result) => {
-      if (err) {
-        return console.error('Error executing query', err.stack);
-      }
-      callback(result.rows);
-    });
+
+//Load a JSON of the full quiz given quiz_id:
+
+const loadWholeQuizJson = (quiz_id, db) => {
+  const queryString = `
+  SELECT questions.question_num, questions.text,
+    (SELECT json_agg(filtered_answers) FROM
+      (SELECT id, answer_text
+        FROM possible_answers
+        WHERE question_id = questions.id
+      ) filtered_answers
+    )
+  AS answer_options
+  FROM questions
+  WHERE questions.quiz_id = $1
+  `
+  const queryParams = [quiz_id];
+  return db.query(queryString, queryParams)
+  .then(res => res.rows)
+  .catch(err => console.error('query error', err.stack));
 };
 
-// const getPossibleAnswers =
+module.exports = { getAttemptData, loadWholeQuizJson }
 
-module.exports = { getAttemptData, getQuestionData }
 
+// const getQuestionData = function(attemptId, db, callback) {
+//       const queryString = `
+//       SELECT questions.id, question_num, text
+//       FROM questions
+//         JOIN quizzes ON questions.quiz_id = quizzes.id
+//         JOIN attempts on quizzes.id = attempts.quiz_id
+//       WHERE attempts.id = $1
+//       ORDER BY question_num
+//       `;
+//       const queryParams = [attemptId];
+//       return db.query(queryString, queryParams)
+//       .then(res => res.rows)
+//       .catch(err => console.log(err))
+//   };
--- routes/result.js
+++ routes/result.js
@@ -5,29 +5,26 @@
 
 const express = require('express');
 const router  = express.Router();
-const { getAttemptData, getQuestionData } = require('../db/helpers/result_helpers.js')
+const { getAttemptData, loadWholeQuizJson } = require('../db/helpers/result_helpers.js')
 
 /* The "db" argument is a Postgres Pool object */
 const resultRouter = (db) => {
   router.get("/:attemptid", (req, res) => {
     const attemptId = req.params.attemptid;
-    const questionCallback = function(questions) {
-      for (const question of questions) {
-        console.log("\n\n\n\n\n\n",question, "\n\n\n\n\n\n")
-      }
-    };
-    console.log(getQuestionData(attemptId, db, questionCallback));
-
     getAttemptData(attemptId, db)
-      .then(results => {
-        if (results) {
-          const templateVars = { results };
+    .then(overallResults => {
+      if (overallResults) {
+        const templateVars = { overallResults };
+        loadWholeQuizJson(overallResults.quiz_id, db)
+        .then(quizJson => {
+          templateVars.quizJson = quizJson;
           res.render("../views/pages/result", templateVars);
-        } else {
-          res.send(`No results found for attempt ID ${attemptId}`);
-        }
-      })
-      .catch(err => console.log(err));
+        })
+      } else {
+        res.send(`No results found for attempt ID ${attemptId}`);
+      }
+    })
+        .catch(err => console.error('Error executing query', err.stack));
   });
   return router;
 };

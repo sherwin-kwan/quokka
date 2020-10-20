fdd141bcd7e4c10b6989ff9ced6d63b0938b9f3d
Will <wdportman@gmail.com>
2020-10-20 18:37:21 +0000

    Remove /user/:id/results and /user:id/quizzes routes in favor of just having /user/:id

--- routes/user.js
+++ routes/user.js
@@ -32,39 +32,6 @@ const userRouter = (db) => {
     res.send('Yay you just registered for Quokka! Welcome to the club!');
   });
 
-
-  // Load results asynchronously
-  router.get('/:id/results', (req, res) => {
-    // db.query(/* INSERT QUERY TO GET PAST RESULTS */)
-    //   .then(data => {
-    //     const users = data.rows;
-    //     res.json({ users });
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //   });
-    res.json({ "notes": "This is the route where user's previous results will pop up",
-      "score": "90%", "status": "this page is just a test" });
-  });
-
-  // Load the user's quizzes asynchronously
-  router.get('/:id/quizzes', (req, res) => {
-    // db.query(/* INSERT QUERY TO GET THE USER'S CREATED QUIZZES */)
-    //   .then(data => {
-    //     const users = data.rows;
-    //     res.json({ users });
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //   });
-    res.json([{ "quiz": "1", "status": "this page is just a test" },
-      { "quiz": "2", "status": "this is just a test" }]);
-  });
-
   // User profile page. Fetches a template (until it's ready, right now it just displays dummy text)
   router.get("/:id", (req, res) => {
     const userId = req.params.id;

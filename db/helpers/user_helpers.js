c234ff3e8a848239967c0a442d033a3950a87421
Will <wdportman@gmail.com>
2020-10-20 19:01:15 +0000

    Set up user_helpers file and improve formatting on result_helpers file

--- db/helpers/user_helpers.js
+++ db/helpers/user_helpers.js
@@ -1,10 +1,16 @@
+//Given user ID, select name
+const getUserName = function(user_id) {
 
+}
 
+//Given a user ID, select quiz title and isPublic for every quiz the user has created:
+const getQuizzesCreated = function(used_id) {
 
+}
 
+//Given a user ID, select the high score & quiz title for each quiz the user has taken:
+const getQuizzesTaken = function(user_id) {
 
+}
 
-
-
-
-module.exports = {  };
+module.exports = { getUserName, getQuizzesCreated, getQuizzesTaken };
--- routes/user.js
+++ routes/user.js
@@ -9,18 +9,7 @@ const router = express.Router();
 /* The "db" argument is a Postgres Pool object */
 const userRouter = (db) => {
 
-  // Login page
-  router.get('/login', (req, res) => {
-    res.send(`This is the login page:
-    <form action='login' method='POST'>
-      <button type='submit'>Log In</buton>
-    </form>`);
-  });
-
-  router.post('/login', (req, res) => {
-    res.send('This is where you either logged in or got a password error');
-  });
-
+  // Register:
   router.get('/register', (req, res) => {
     res.send(`This is the registration page:
     <form action='register' method='POST'>
@@ -32,7 +21,21 @@ const userRouter = (db) => {
     res.send('Yay you just registered for Quokka! Welcome to the club!');
   });
 
-  // User profile page. Fetches a template (until it's ready, right now it just displays dummy text)
+
+  // Login:
+  router.get('/login', (req, res) => {
+    res.send(`This is the login page:
+    <form action='login' method='POST'>
+      <button type='submit'>Log In</buton>
+    </form>`);
+  });
+
+  router.post('/login', (req, res) => {
+    res.send('This is where you either logged in or got a password error');
+  });
+
+
+  // User profile:
   router.get("/:id", (req, res) => {
     const userId = req.params.id;
     res.send(`This is the future profile page for user ${userId}`);

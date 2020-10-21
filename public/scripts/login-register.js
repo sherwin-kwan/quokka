b5251bbb1aa6aeca64314a0a7993024e48b7b326
Sherwin <sherwin.kwan92@gmail.com>
2020-10-21 13:47:41 -0700

    test

--- public/scripts/login-register.js
+++ public/scripts/login-register.js
@@ -22,44 +22,43 @@ $(() => {
     $loginForm.hide();
   });
 
-  $loginForm.on('submit', (e) => {
-    e.preventDefault();
-    // VAlidation code here
-    //
-    const valid = true;
-    //
-    //
-    if (valid) {
-      $.ajax('/user/login', { method: 'POST', data: $loginForm.serialize() })
+  const verifyInputNotEmpty = ($form) => {
+    for (input of $form.find(`input[type='text']`)) {
+      console.log($(input).val());
+      if ($(input).val().length === 0) {
+        throw new Error('All fields are required and cannot be empty');
+      }
+    }
+    if ($form.find(`input[type='password']`).val().length < 5) {
+      throw new Error('Please choose a password at least 5 characters long');
+    }
+  };
+
+  const ajaxLoginHandler = ($form, procedure) => {
+    try {
+      verifyInputNotEmpty($form);
+      $.ajax(`/user/${procedure}`, { method: 'POST', data: $form.serialize() })
         .then(// Successful
-          (data, status, xhr) => {
+          () => {
             window.location.href = '/';
           }
-        )
-        .catch(err => {
-          alert(err.responseText);
-        });
+        ).catch(err => {
+          throw new Error(err.responseText);
+        })
+    } catch (err) {
+      console.log(err.message);
+      $form.find('div.error-message').append(err.message);
     };
+  };
+
+  $loginForm.on('submit', (e) => {
+    e.preventDefault();
+    ajaxLoginHandler($registerForm, 'login');
   });
 
   $registerForm.on('submit', (e) => {
     e.preventDefault();
-    // VAlidation code here
-    //
-    const valid = true;
-    //
-    //
-    if (valid) {
-      $.ajax('/user/register', { method: 'POST', data: $registerForm.serialize() })
-        .then(// Successful
-          (data, status, xhr) => {
-            window.location.href = '/';
-          }
-        )
-        .catch(err => {
-          alert(err.responseText);
-        });
-    };
+    ajaxLoginHandler($registerForm, 'register');
   });
 
 })

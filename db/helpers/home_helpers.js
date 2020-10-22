b824106e92411bb89533cd2a9623a2f04455ed49
Sherwin <sherwin.kwan92@gmail.com>
2020-10-22 10:52:19 -0700

    new functions for sorting homepage quizzes

--- db/helpers/home_helpers.js
+++ db/helpers/home_helpers.js
@@ -1,12 +1,55 @@
-const format = require('pg-format');
+// Helper functions for home page
 
-//Queries for the latest 10 
-const loadPublicQuizzes = (db) => {
-  return db.query(`SELECT id, title
-  FROM quizzes
-  WHERE is_public = true
-  ORDER BY created_at DESC
-  LIMIT 16;`)
+//Queries for the latest <limit> results in <db>, sorted by <sort>
+const loadPublicQuizzes = (sort, limit, db) => {
+  let query;
+  switch (sort) {
+    case 'recent':
+      query = `SELECT id, title
+      FROM quizzes
+      WHERE is_public = true
+      ORDER BY created_at DESC
+      LIMIT $1`;
+      break;
+    case 'popular':
+      query = `SELECT quizzes.id, title, COUNT(attempts.id) AS submissions
+      FROM quizzes
+      JOIN attempts ON quizzes.id = attempts.quiz_id
+      WHERE is_public = true
+      GROUP BY quizzes.id
+      ORDER BY submissions DESC
+      LIMIT $1`;
+      break;
+    case 'long':
+      query = `SELECT quizzes.id, title, COUNT(questions.id) AS num_questions
+      FROM quizzes
+      JOIN questions ON quizzes.id = questions.quiz_id
+      WHERE is_public = true
+      GROUP BY quizzes.id
+      ORDER BY num_questions DESC
+      LIMIT $1`;
+      break;
+    case 'short':
+      query = `SELECT quizzes.id, title, COUNT(questions.id) AS num_questions
+      FROM quizzes
+      JOIN questions ON quizzes.id = questions.quiz_id
+      WHERE is_public = true
+      GROUP BY quizzes
+      ORDER BY num_questions
+      LIMIT $1`;
+      break;
+    case 'trending':
+      query = `SELECT quizzes.id, title, COUNT(attempts.id) AS recent_submissions
+      FROM quizzes
+      JOIN attempts ON quizzes.id = attempts.quiz_id
+      WHERE is_public = true
+      AND attempts.finished_at > 
+      GROUP BY quizzes
+      ORDER BY num_questions
+      LIMIT $1`;
+      break;
+  };
+  return db.query(query, [limit])
   .then(results => {
     return results.rows;
   })
